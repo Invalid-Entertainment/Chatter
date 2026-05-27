40cdf40d2d156bec155919436733ecf5d8427571
ThatCreeper <creeperhostanimations@gmail.com>
2026-05-27 16:07:04 -0500

    feat: create a real and coherent update flow

--- Chatter/Program.cs
+++ Chatter/Program.cs
@@ -1,10 +1,14 @@
 ﻿Console.WriteLine($"My Version:\t{Generated.REV}");
 Console.WriteLine($"Latest Version:\t{ReleaseChecker.GetLatestRelease()}");
+
+ReleaseChecker.CheckForUpdates();
+
 Console.ReadLine();
 
 class ReleaseChecker
 {
     const string API_PATH = "https://api.github.com/repos/Invalid-Entertainment/Chatter/releases/latest";
+    const string REL_PATH = "https://github.com/Invalid-Entertainment/Chatter/releases/latest";
 
     public static string? GetLatestRelease()
     {
@@ -22,5 +26,25 @@ class ReleaseChecker
             Console.WriteLine(e.ToString());
             return null;
         }
+    }
+
+    public static void OpenNewRelease(string newVersion)
+    {
+        var result = Windows.Win32.PInvoke.MessageBox(Windows.Win32.Foundation.HWND.Null, $"An update is available! {Generated.REV} (current) -> {newVersion} (new). Would you like to update?", "Update available!", Windows.Win32.UI.WindowsAndMessaging.MESSAGEBOX_STYLE.MB_YESNO);
+
+        if (result != Windows.Win32.UI.WindowsAndMessaging.MESSAGEBOX_RESULT.IDYES)
+            return;
+
+        Windows.Win32.PInvoke.MessageBox(Windows.Win32.Foundation.HWND.Null, "Chatter will now open both the new release download location and the current installed Chatter.", "Opening new release", Windows.Win32.UI.WindowsAndMessaging.MESSAGEBOX_STYLE.MB_OK);
+
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { FileName = REL_PATH, UseShellExecute = true });
+        System.Diagnostics.Process.Start("explorer", ".");
+    }
+
+    public static void CheckForUpdates()
+    {
+        string? latest = GetLatestRelease();
+        if (latest != null && latest != Generated.REV)
+            OpenNewRelease(latest);
     }
 }
