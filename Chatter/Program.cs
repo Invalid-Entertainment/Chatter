875884b292108a7036f5516efd384f02be6d781e
ThatCreeper <creeperhostanimations@gmail.com>
2026-05-27 15:22:53 -0500

    feat: allow fetching latest release

--- Chatter/Program.cs
+++ Chatter/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(ReleaseChecker.GetLatestRelease());
+Console.ReadLine();
+
+class ReleaseChecker
+{
+    const string API_PATH = "https://api.github.com/repos/Invalid-Entertainment/Chatter/releases/latest";
+
+    public static string? GetLatestRelease()
+    {
+        try
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Chatter");
+            string res = client.GetStringAsync(API_PATH).Result;
+            res = res[(res.IndexOf(",\"tag_name\":\"") + 13)..];
+            res = res[..res.IndexOf('"')];
+            return res;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            return null;
+        }
+    }
+}
